2cdc9fadc58ed96663e122de0e64b8619b8211c2
UnionGhost <takeofme@gmail.com>
2022-04-10 20:03:45 +0300

    рефакторинг

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 import { createMarker, mainMarkerDefaultCoordinate } from './map.js';
 import { showAlert } from './utils.js';
 import { setOfferForm } from './offer-form.js';
+import { clearNoUiSlider } from './price-slider.js';
 import { getData } from './api.js';
 
 getData(createMarker, showAlert);
 
-setOfferForm(mainMarkerDefaultCoordinate);
+setOfferForm(() => {
+  mainMarkerDefaultCoordinate();
+  clearNoUiSlider();
+});
