d58cea02cc201cc2d07db3daa7f9dc665e9ecd1b
UnionGhost <takeofme@gmail.com>
2022-04-17 00:16:32 +0300

    Поменял пару функций на стрелочные

--- js/price-slider.js
+++ js/price-slider.js
@@ -18,12 +18,8 @@ noUiSlider.create(priceSlider, {
   step: 1,
   connect: 'lower',
   format: {
-    to: function (value) {
-      return value.toFixed(0);
-    },
-    from: function (value) {
-      return parseFloat(value);
-    },
+    to: (value) => value.toFixed(0),
+    from: (value) => parseFloat(value),
   },
 });
 
